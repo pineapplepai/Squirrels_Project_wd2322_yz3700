e63bb5950899207357c19cae9df34db8ad38f1e4
pineapplepai <weiran.duan@hotmail.com>
2019-12-07 16:08:29 -0500

    Mysite urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from sightings.views import sightings_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('map/', include('map.urls')),
+    path('admin/', admin.site.urls),
+]
 ]
