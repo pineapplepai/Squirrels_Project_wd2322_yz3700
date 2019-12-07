d3bb410f9e30c268e6b97ed62a9261e7934fe5bb
pineapplepai <weiran.duan@hotmail.com>
2019-12-07 15:32:53 -0500

    Map app updated

--- mysite/map/views.py
+++ mysite/map/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from sightings.models import Squirrel
+
+def map(request, *args, **kwargs):
+    sightings = Squirrel.objects.all()[:25]
+    context = {
+	'sigtings':sightings
+    }
+    return render(request, 'map/map.html',context)
